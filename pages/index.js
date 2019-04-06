fe46541c9f5cbab10d10ab985427e5a4e6601989
Sebastian De Deyne <sebastiandedeyne@gmail.com>
2019-04-06 19:33:06 +0200

    Add demo comments

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,13 @@ import { CocktailsPage, Cocktail, Search } from "../components";
 export default function Index() {
   const search = "margherita";
 
+  // Make `search` stateful
+
+  // Update the page title with the search string
+
+  // Use thecockataildb.com to search
+  // https://www.thecocktaildb.com/api/json/v1/1/search.php?s=
+
   return (
     <CocktailsPage>
       <Search value={search} />
